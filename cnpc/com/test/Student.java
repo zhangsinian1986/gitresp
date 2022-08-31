5556be010fc65428a8b8c6c7d530a28307558a82
zhangsinian1986 <qzhangkai1986@163.com>
2022-08-31 21:56:31 +0800

    Changes

---
+++ cnpc/com/test/Student.java
@@ -0,0 +1,40 @@
+package cnpc.com.test;
+
+public class Student {
+    String name;
+    int age;
+    String description;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, String description) {
+        this.name = name;
+        this.age = age;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
