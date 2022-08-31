0e26bea7ad58e8537aaa0efe7e6ea93a3149595e
zhangsinian1986 <qzhangkai1986@163.com>
2022-08-31 21:58:51 +0800

    Changes

---
+++ cnpc/com/test/StuTest.java
@@ -0,0 +1,8 @@
+package cnpc.com.test;
+
+public class StuTest {
+    public static void main(String[] args) {
+        Student stu1 = new Student("zhangsan", 12, "三好学生");
+        System.out.println(stu1);
+    }
+}
